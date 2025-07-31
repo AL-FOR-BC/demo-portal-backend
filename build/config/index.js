c5861b8f03c354b6d104c4896631dcdc53780fc7
CODE-WITH-JOM <mukiibijoseph19@gmail.com>
2025-07-31 13:00:31 +0300

    solving cors

--- build/config/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APP_SECRET = exports.PORT = void 0;
-exports.PORT = process.env.PORT || 5001;
-exports.APP_SECRET = "Our_APP_Secret";
-//# sourceMappingURL=index.js.map
